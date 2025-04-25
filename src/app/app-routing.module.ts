fc91daaa8175138e9b38e1b47fa2e4e395c87c2a
batoolamer26 <batoolamer260@gmail.com>
2025-04-25 16:35:57 +0300

    addImages-routing-and changeDesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,34 +2,39 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserTypeGuard } from './guards/user-type.guard';
+import { HomePageComponent } from './page/home-page/home-page.component';
 
 const routes: Routes = [
+  { path: '', component: HomePageComponent },
   {
     path: 'login',
-    component:LoginComponent
+    component: LoginComponent,
   },
   {
-    path:'admin',
-    loadChildren:()=> import('./modules/admin/admin.module').then(m => m.AdminModule),
-    canMatch:[UserTypeGuard],
-    data:{expectedType:'admin'}
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then((m) => m.AdminModule),
+    canMatch: [UserTypeGuard],
+    data: { expectedType: 'admin' },
   },
   {
-    path:'doctor',
-    loadChildren:()=> import('./modules/doctor/doctor.module').then(m => m.DoctorModule),
-    canMatch:[UserTypeGuard],
-    data:{expectedType:'doctor'}
+    path: 'doctor',
+    loadChildren: () =>
+      import('./modules/doctor/doctor.module').then((m) => m.DoctorModule),
+    canMatch: [UserTypeGuard],
+    data: { expectedType: 'doctor' },
   },
   {
-    path:'patient',
-    loadChildren:()=> import('./modules/patient/patient.module').then(m => m.PatientModule),
-    canMatch:[UserTypeGuard],
-    data:{expectedType:'patient'}
-  }
+    path: 'patient',
+    loadChildren: () =>
+      import('./modules/patient/patient.module').then((m) => m.PatientModule),
+    canMatch: [UserTypeGuard],
+    data: { expectedType: 'patient' },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
