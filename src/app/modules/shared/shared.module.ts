12ae2022b9852f1f17b2fa2ea762ea5e4e007297
rana-asaireh <rana.asaireh@gmail.com>
2025-04-26 22:57:56 +0300

    Update shared.module.ts

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AuthHeaderComponent } from './components/auth-header/auth-header.component';
 import { AuthFooterComponent } from './components/auth-footer/auth-footer.component';
 import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
 import { AuthSideBarComponent } from './components/auth-side-bar/auth-side-bar.component';
-import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +14,10 @@ import { RouterModule } from '@angular/router';
     AuthLayoutComponent,
     AuthSideBarComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   exports: [AuthLayoutComponent],
 })
 export class SharedModule {}
