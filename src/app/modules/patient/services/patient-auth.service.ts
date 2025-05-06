030090ce867488c19a1c821aa4d2f6de6023c3f4
RahafJazar <rahafjazar73@gmail.com>
2025-05-06 14:18:03 +0300

    patient

---
+++ src/app/modules/patient/services/patient-auth.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map, Observable, retry, switchMap, Unsubscribable } from "rxjs";
+
+import { FormControl } from "@angular/forms";
+import { Patient } from "../../shared/models/patient.model";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientAuthService {
+
+
+  baseUrl = 'http://localhost:3000/patient'
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  getPatientsList(): Observable<Patient[]> {
+    return this.http.get<Patient[]>(this.baseUrl)
+  }
+
+  //check if email of user  is exist 
+  checkEmailExist(email: string): Observable<boolean> {
+    return this.http.get<Patient[]>(this.baseUrl + '?email=' + email).pipe(
+      map((patients) => patients.length > 0) //true if at least one match
+    )
+
+  }
+
+
+  addPatient(patient: Patient): Observable<Patient> {
+    return this.http.post<Patient>(this.baseUrl, patient);
+  }
+
+
+  getCurrentPatientId(email: string): Observable<string | undefined> {
+    return this.http.get<Patient[]>(`${this.baseUrl}?email=${email}`).pipe(
+      map(patients => {
+        if (patients.length > 0) {
+          return patients[0].id
+        } else {
+          throw new Error('Patient not found');
+        }
+      })
+    )
+  }
+
+
+  //
+
+
+
+}
---
+++ src/app/modules/shared/models/patient.model.ts
@@ -0,0 +1,12 @@
+
+export interface Patient {
+    id?: string,
+    name: string,
+    email: string,
+    phone: number,
+    gender: string,
+    dob: Date
+
+
+
+}
