8983d32e396883a92d56dbc46fadad87a77a2702
batoolamer26 <batoolamer260@gmail.com>
2025-05-08 17:16:19 +0300

    doctor and home

--- src/app/modules/doctor/doctor-routing.module.ts
+++ src/app/modules/doctor/doctor-routing.module.ts
@@ -3,38 +3,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { DoctorLayoutComponent } from './components/doctor-layout/doctor-layout.component';
 import { ViewAppointmentListComponent } from './components/view-appointment-list/view-appointment-list.component';
-import { PatientListComponent } from './components/patient-list/patient-list.component';
 import { AppointmentDetailsComponent } from './components/appointment-details/appointment-details.component';
 import { PatientDetailsComponent } from './components/patient-details/patient-details.component';
+import { PatientListComponent } from './components/patient-list/patient-list.component';
 
 const routes: Routes = [
-  
-      {
-        path: '',
-        component: ProfileComponent
-      },
-      {
-        path:'appointment',
-        component:ViewAppointmentListComponent
-      },
-      {
-        path:'patients',
-        component:PatientListComponent
-      },
-      {
-        path:'appointment-details/:id',
-        component:AppointmentDetailsComponent
-      },
-      {
-        path:'patient-details',
-        component:PatientDetailsComponent
-      }
-    ]
-  
+  {
+    path: '',
+    component: DoctorLayoutComponent,
+    children: [
+      { path: '', component: ProfileComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'appointments/:id', component: ViewAppointmentListComponent },
+      { path: 'patients', component: PatientListComponent },
 
+      {
+        path: 'appointment-details/:id',
+        component: AppointmentDetailsComponent,
+      },
+      { path: 'patient-details', component: PatientDetailsComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class DoctorRoutingModule {}
--- src/app/modules/shared/services/user.service.ts
+++ src/app/modules/shared/services/user.service.ts
@@ -4,20 +4,18 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { map, Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-
-
-  private baseUrl = 'http://localhost:3000/users'; 
+  private baseUrl = 'http://localhost:3000/users';
   private patientsBaseUrl = 'http://localhost:3000/patient';
-  private doctorsBaseUrl = 'http://localhost:3000/doctor'; 
-  constructor(private http: HttpClient, private router: Router) { }
+  private doctorsBaseUrl = 'http://localhost:3000/doctor';
+  constructor(private http: HttpClient, private router: Router) {}
   getUser(email: string, password: string): Observable<User[]> {
-    return this.http.get<User[]>(`${this.baseUrl}?email=${email}&password=${password}`)
+    return this.http.get<User[]>(
+      `${this.baseUrl}?email=${email}&password=${password}`
+    );
   }
   getCurrentUser(): User {
     return JSON.parse(localStorage.getItem('currentUser') || '{}');
@@ -26,29 +24,32 @@ export class UserService {
     const user = this.getCurrentUser();
     return user?.type || '';
   }
+
+  getTypeUser() {
+    return JSON.parse(localStorage.getItem('typeUser') || '');
+  }
   addUserDoctor(doctor: User): Observable<User> {
     return this.http.post<User>(`${this.baseUrl}`, doctor);
   }
 
   getUserDoctorByEmail(email: string): Observable<User> {
-    return this.http.get<User[]>(`${this.baseUrl}?email=${email}`).pipe(
-      map(users => users[0])
-    );
+    return this.http
+      .get<User[]>(`${this.baseUrl}?email=${email}`)
+      .pipe(map((users) => users[0]));
   }
-  updateUserDoctorByid(id?: string, updatedUserDoctor?: User): Observable<User> {
+  updateUserDoctorByid(
+    id?: string,
+    updatedUserDoctor?: User
+  ): Observable<User> {
     return this.http.put<User>(`${this.baseUrl}/${id}`, updatedUserDoctor);
   }
   deleteUserDoctorByid(id?: string): Observable<User> {
     return this.http.delete<User>(`${this.baseUrl}/${id}`);
-     
   }
 
-
   addUser(user: User): Observable<User> {
-
     return this.http.post<User>(this.baseUrl, user).pipe(
       map((userWithId) => {
-
         // Remove the 'id' field from the response before returning it
         this.http.delete<void>(`${this.baseUrl}/${user.id}`);
         const { id, ...userWithoutId } = userWithId; // destructure and exclude 'id'
@@ -56,26 +57,28 @@ export class UserService {
       })
     );
   }
-  getCurrentPatientId(): any { 
+  getCurrentPatientId(): any {
     const user = this.getCurrentUser();
-  
+
     return user?.id || null;
   }
-  getCurrentDoctorId(): any { 
+  getCurrentDoctorId(): any {
     const user = this.getCurrentUser();
-  
+
     return user?.id || null;
   }
-  getPatientByEmail(email:string):Observable<any>{
-    return this.http.get<any>(`${this.patientsBaseUrl}?email=${email}`)
+  getPatientByEmail(email: string): Observable<any> {
+    return this.http.get<any>(`${this.patientsBaseUrl}?email=${email}`);
   }
-  
-  getDoctorByEmail(email: string) :Observable<any> {
-    return this.http.get<any>(`${this.doctorsBaseUrl}?email=${email}`)
+
+  getDoctorByEmail(email: string): Observable<any> {
+    return this.http.get<any>(`${this.doctorsBaseUrl}?email=${email}`);
   }
 
   getUserByPatientId(patientId: string): Observable<User[]> {
-    return this.http.get<User[]>(`${this.baseUrl}?id=${patientId}&type=patient`);
+    return this.http.get<User[]>(
+      `${this.baseUrl}?id=${patientId}&type=patient`
+    );
   }
   getUserByEmail(email: string): Observable<User[]> {
     return this.http.get<User[]>(`${this.baseUrl}?email=${email}`);
@@ -84,4 +87,3 @@ export class UserService {
     return this.http.put<User>(`${this.baseUrl}/${userData.id}`, userData);
   }
 }
-
